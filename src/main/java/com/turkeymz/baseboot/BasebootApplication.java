954ccb94eabef9c9877c016313c2fe05b2033bd1
richard <v-rxu@expeida.com>
2020-06-15 14:49:50 +0800

    init project

---
+++ src/main/java/com/turkeymz/baseboot/BasebootApplication.java
@@ -0,0 +1,13 @@
+package com.turkeymz.baseboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasebootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasebootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/turkeymz/baseboot/BasebootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkeymz.baseboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasebootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

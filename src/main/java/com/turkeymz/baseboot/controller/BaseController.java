39d36c984ccf235a0def15a1ca16089ef4a9fcb9
richard <v-rxu@expeida.com>
2020-06-19 10:36:13 +0800

    add log matrix

--- src/main/java/com/turkeymz/baseboot/controller/BaseController.java
+++ src/main/java/com/turkeymz/baseboot/controller/BaseController.java
@@ -3,6 +3,8 @@ package com.turkeymz.baseboot.controller;
 import com.turkeymz.baseboot.entity.APIResultBody;
 import com.turkeymz.baseboot.exception.SysException;
 import com.turkeymz.baseboot.exception.code.HttpExceptionCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,9 +12,15 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class BaseController {
 
+    private final Logger logger = LoggerFactory.getLogger(BaseController.class);
+
     @GetMapping("/hello/{name}")
     public APIResultBody sayHello(@PathVariable("name") String name){
+
+        logger.info("input: {}",name);
+
         String result = "Hello " + name + ", here is base boot.";
+
         if("error".equals(name)){
             throw new SysException(HttpExceptionCode.SERVER_BUSY);
         }
